5117229013ac254ac129cd38fd1633dd9c013311
Jacky20200711 <fewer135+1@gmail.com>
2021-07-23 22:09:48 +0800

    完成匯出匯入的畫面

---
+++ JobFilter2/Controllers/BackupController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JobFilter2.Controllers
+{
+    public class BackupController : Controller
+    {
+        public IActionResult Export()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Export(IFormCollection PostData)
+        {
+            // 檢查路徑
+
+            return View();
+        }
+
+        public IActionResult Import()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Import(IFormCollection PostData)
+        {
+            // 檢查路徑
+
+            return View();
+        }
+    }
+}
