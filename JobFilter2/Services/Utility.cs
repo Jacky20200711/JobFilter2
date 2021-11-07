f82800cc75d230ec7bc02a9318f39c40383906eb
Jacky20200711 <fewer135+1@gmail.com>
2021-11-07 09:18:28 +0800

    refactor & 新增返回封鎖列表的按鈕

---
+++ JobFilter2/Services/Utility.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace JobFilter2.Services
+{
+    public static class Utility
+    {
+        /// <summary>
+        /// 定義封鎖公司的可能原因
+        /// </summary>
+        public static List<string> BlockReason = new List<string>
+        {
+            "接案或派遣",
+            "交通不方便",
+            "技能樹不符合",
+            "看完評論後失去興趣",
+            "看完職缺描述或公司介紹後失去興趣",
+            "看完面試說明或親自面過後失去興趣",
+        };
+    }
+}
