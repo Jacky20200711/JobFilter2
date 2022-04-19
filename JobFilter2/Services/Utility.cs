a9edc237e544c95f582cd72222fe599bcaa3b1be
Jacky20200711 <fewer135+1@gmail.com>
2022-04-19 18:58:24 +0800

    調整封鎖公司的可能原因

--- JobFilter2/Services/Utility.cs
+++ JobFilter2/Services/Utility.cs
@@ -5,7 +5,7 @@ namespace JobFilter2.Services
     public static class Utility
     {
         /// <summary>
-        /// 定義封鎖公司的可能原因
+        /// 列舉封鎖公司的可能原因
         /// </summary>
         public static List<string> BlockReason = new List<string>
         {
@@ -17,7 +17,8 @@ namespace JobFilter2.Services
             "上班時間9.5小時",
             "職缺描述寫太爛",
             "對招募流程不滿意",
-            "查看職缺後失去興趣",
+            "對這類產業沒興趣",
+            "查看職缺後覺得很雷",
             "親自面過後失去興趣",
         };
     }
